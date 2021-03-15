850b626e95fb581e30f8683ff9e9b85b1d32ed6e
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-03-16 03:23:44 +0600

    refactor: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+var numString = number.toString();
+var onesHundred = +(numString.substring(1));
+var single = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+var double = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+var triple = ' hundred';
+var str = '';
+
+if (number == 0) {
+    return 'zero';
+}
+
+if (number < 20) {
+  return single[number].trim(); 
+}
+
+if (numString.length == 2) {
+str += double[+(numString[0])];
+str += single[+(numString[1])];
+  return str.trim();
+}
+
+str =  single[+(numString[0])] + triple;
+if (onesHundred < 20){
+  str += single[onesHundred];
+  return str.trim(); 
+} else {
+  str += double[+(numString[1])];
+  str += single[+(numString[2])];
+  return str.trim();
+}
+
 }
